ecc8fb68c8e51f5c4fe97d1935fa7f1aeafc830b
retornam <retornam@users.noreply.github.com>
2025-11-28 22:32:37 -0800

    (chg): use https://api.ipify.org/?format=json
    switch to using ipify.org instead of httpbin.org/ip
    
    Signed-off-by: Raymond Etornam <retornam@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 
 
 /*
-Given http://httpbin.org/ip as an API endpoint that returns your current IP,
+Given http://httpbin.org/ip or https://api.ipify.org/?format=json
+as an API endpoint that returns your current IP,
 write a JavaScript function that makes a call to the endpoint, gets the value
 returned and displays it on an HTML page.
 
@@ -16,7 +17,7 @@ How will you test this? */
 //   if(xhr.status === 200){
 //     result = JSON.parse(xhr.responseText);
 //   }
-  
+
 //   console.log("Log result");
 //   console.log(result);
 //   var inner = document.getElementById("inner");
@@ -25,12 +26,12 @@ How will you test this? */
 
 $(function() {
   var result;
-  $.getJSON("https://httpbin.org/ip?format=json").done( function(data) {
+  $.getJSON("https://api.ipify.org/?format=json").done( function(data) {
     console.log("Log Result")
     result = JSON.stringify(data);
   }).then(function(result) {
-    $('#inner > p').text("IP: " + result.origin);
-    $('#inner > span').text("CIDR: " + result.origin + "/32");
+    $('#inner > p').text("IP: " + result.ip);
+    $('#inner > span').text("CIDR: " + result.ip + "/32");
   })
   const browser = bowser.parse(window.navigator.userAgent);
   var os = browser.os.name.concat(" ", browser.os.version);
